1f9dde9b4f84458be519f20c1926d0674876ed6c
ShenShizhe <3093326255@qq.com>
2022-12-10 10:52:28 +0800

    Update

---
+++ Learning/Learning/74函数作业.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include <Windows.h>
+#include <time.h>
+#include "Function.h"
+int main(void)
+{
+
+	return 0;
+}
